5591223db92bb3716e61937a0fa12cf7e751beda
MerKi1306 <pansowa1306@gmail.com>
2023-12-14 01:23:46 +0100

    added repositories

---
+++ src/main/java/com/example/PizzaParty/repositories/DrinkRepository.java
@@ -0,0 +1,9 @@
+package com.example.PizzaParty.repositories;
+
+import com.example.PizzaParty.models.Drink;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface DrinkRepository extends JpaRepository<Drink,Long> {
+
+}
---
+++ src/main/java/com/example/PizzaParty/repositories/PizzaRepository.java
@@ -0,0 +1,11 @@
+package com.example.PizzaParty.repositories;
+
+
+import com.example.PizzaParty.models.Pizza;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PizzaRepository extends JpaRepository<Pizza,Long> {
+
+}
